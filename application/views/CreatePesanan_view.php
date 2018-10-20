605e4f37134d81c3a500ad75b40df8f254ac1f8c
MARDIANAKOM\Mardiana <now101246@gmail.com>
2018-10-20 09:13:41 +0700

    no message

--- application/views/CreatePesanan_view.php
+++ application/views/CreatePesanan_view.php
@@ -10,10 +10,11 @@
           <p>
             
             <div class="container">
+
               <th>
-            <div class="container">
+                
             <div class="form-group row">
-            <div class="col-xs-4">
+            <div class="col-xs-4" >
             <form method="post" action="input">   
             <label  for="nama">Nama Pemesan:</label>
             <input class="form-control" placeholder="Masukan Nama" type="text" name="nama_pemesan" value="<?php if(isset($data)) { echo $data[0]->nama_pemesan; } ?>">
@@ -55,9 +56,7 @@
         </div>
       </div>
 
-              <input type="submit" class="btn btn-success" name="btnTambah" value="Simpan"/>
-              <a class="btn btn-warning" href="<?php echo base_url()?>Pesanan">Kembali</a>
-
+              
 
               </div>
             </td>
@@ -72,3 +71,79 @@
       </div>
     </div>
 
+              <div class="row">
+            <div class="col-lg-12">
+                    <a href="#" data-toggle="modal" data-target="#largeModal" class="pull-right"><small>Cari Roti</small></a>
+                </h1> 
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Projects Row -->
+        <div class="row">
+            <div class="col-lg-12">
+            <form action="<?php echo base_url().'Penjualan/add_to_cart'?>" method="post">
+            <table>
+                <tr>
+                    <th>ID Roti</th>
+                </tr>
+                <tr>
+                    <th><input type="text" name="id_roti" id="id_roti" class="form-control input-sm"></th>                     
+                </tr>
+                    <div id="detail_roti" style="position:absolute;">
+                    </div>
+            </table>
+             </form>
+            <table class="table table-bordered table-condensed" style="font-size:11px;margin-top:10px;">
+                <thead>
+                    <tr>
+                        <th>ID Roti</th>
+                        <th>Nama Roti</th>
+                        <th style="text-align:center;">Harga(Rp)</th>
+                        <th style="text-align:center;">Qty</th>
+                        <th style="text-align:center;">Sub Total</th>
+                        <th style="width:100px;text-align:center;">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($this->cart->contents() as $items): ?>
+                    <?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
+                    <tr>
+                         <td><?=$items['id'];?></td>
+                         <td><?=$items['name'];?></td>
+                         <td style="text-align:right;"><?php echo number_format($items['amount']);?></td>
+                         <td style="text-align:center;"><?php echo number_format($items['qty']);?></td>
+                         <td style="text-align:right;"><?php echo number_format($items['subtotal']);?></td>
+                         <td style="text-align:center;"><a href="<?php echo base_url().'Penjualan/remove/'.$items['rowid'];?>" class="btn btn-warning btn-xs"><span class="fa fa-close"></span> Batal</a></td>
+                    </tr>
+                    
+                    <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <form action="<?php echo base_url().'Penjualan/simpan_penjualan'?>" method="post">
+            <table>
+                <tr>
+                    <td style="width:760px;" rowspan="2"><button type="submit" class="btn btn-info btn-lg"> Simpan</button></td>
+                    <th style="width:140px;">Total(Rp)</th>
+                    <th style="text-align:right;width:140px;"><input type="text" name="total2" value="<?php echo number_format($this->cart->total());?>" class="form-control input-sm" style="text-align:right;margin-bottom:5px;" readonly></th>
+                    <input type="hidden" id="total" name="total" value="<?php echo $this->cart->total();?>" class="form-control input-sm" style="text-align:right;margin-bottom:5px;" readonly>
+                </tr>
+                <tr>
+                    <th>Bayar(Rp)</th>
+                    <th style="text-align:right;"><input type="text" id="jml_uang" name="jml_uang" class="jml_uang form-control input-sm" style="text-align:right;margin-bottom:5px;" required></th>
+                    <input type="hidden" id="jml_uang2" name="jml_uang2" class="form-control input-sm" style="text-align:right;margin-bottom:5px;" required>
+                </tr>
+                <tr>
+                    <td></td>
+                    <th>Kembalian(Rp)</th>
+                    <th style="text-align:right;"><input type="text" id="kembalian" name="kembalian" class="form-control input-sm" style="text-align:right;margin-bottom:5px;" required></th>
+                </tr>
+
+            </table>
+            </form>
+            <hr/>
+        </div>
+            
+            <input type="submit" class="btn btn-success" name="btnTambah" value="Simpan"/>
+              <a class="btn btn-warning" href="<?php echo base_url()?>Pesanan">Kembali</a>
